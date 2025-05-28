34bdd08f105f1deed6b9bd02099b653aebf53316
gitxsonu <sonu546gowda@gmail.com>
2025-05-28 14:36:33 +0530

    hi

---
+++ vote.js
@@ -0,0 +1,13 @@
+function vote(){
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var res = document.getElementById("res");
+
+    if(age<18){
+        res.innerHTML = "You are not eligible 😒";
+    }
+
+    else{
+        res.innerHTML = "You are eligible 👀";
+    }
+}
